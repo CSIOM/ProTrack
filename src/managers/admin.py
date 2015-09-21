f295918524bccb4e6dd890e4a9226c2bc4cb89d0
Satinder <satinder@csiom.com>
2015-09-21 13:02:58 +0530

    Beta 0.1

--- src/managers/admin.py
+++ src/managers/admin.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
-#
-# (c) 2015 CSIOM, http://www.csiom.com
-#
-# This file is part of DeeDee project.
-#
-# DeeDee is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# DeeDee is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with DeeDee.  If not, see <http://www.gnu.org/licenses/>.
-#
-#%% Authors %%
-#Jasvir Singh Grewal <js@csiom.com>
-#Aseem Mittal <aseemmittal@csiom.com>
+"""
+This file is part of piPi project.
+
+Copyright (C) 2015 CSIOM, http://www.csiom.com
+Authors: The Csiom Team
+
+piPi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 
 """
 %% src/managers/admin.py %%
--- src/urls.py
+++ src/urls.py
@@ -1,3 +1,24 @@
+"""
+This file is part of piPi project.
+
+Copyright (C) 2015 CSIOM, http://www.csiom.com
+Authors: The Csiom Team
+
+piPi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from src.main.views import MainIndex, SignUp, ViewReport
